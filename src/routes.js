8a57d9bbc0ec6f45a881a7283ec1bbe7447b4e1b
ArielBetti <ariel.betti@gmail.com>
2019-11-03 03:04:55 -0300

    Passando rota de loader

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import LoadUser from './pages/LoadUser';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login}/>
-                <Route path="/home" exact component={Home}/>
+                <Route path="/home" component={Home}/>
+                <Route path="/LoadUser" component={LoadUser}/>
             </Switch>
         </BrowserRouter>
     )
