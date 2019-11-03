d67408d39cacec5eaa4cdcd64a168e236dfc6df9
ArielBetti <ariel.betti@gmail.com>
2019-11-03 03:07:25 -0300

    Restruturando Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './style.css';
 import Logo from '../../assets/spotify.svg';
+import Routes from '../../routes';
 
-export default function Login() {
+export default function() {
     return (
         <div className="AppLogin">
             <div className="logo"><img src={Logo} alt="Xpotify logo" />
                 <h1 className="TitleLogin">Xpotify</h1>
             </div>
             {/* <a className="LoginBtn" href='https://xpotify-auth.herokuapp.com/login' >Login com Spotify </a>  */}
-            <a className="LoginBtn" href='http://localhost:8080/login' >Login com Spotify </a>
+            <a className="LoginBtn" href="http://localhost:8080/login" >Login com Spotify </a>
         </div>
     );
 }
