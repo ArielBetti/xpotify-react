d74765e6c87888cdab9d9081f00559389238535d
ArielBetti <ariel.betti@gmail.com>
2019-11-03 03:07:05 -0300

    Restryturando Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,67 +6,8 @@ import search from '../../assets/search-white.svg';
 
 export default function Home() {
 
-    function TokenRemain() {
-
-        function getHashParams() {
-            let hashParams = {};
-            let e, r = /([^&;=]+)=?([^&;]*)/g,
-                q = window.location.hash.substring(1);
-            while (e = r.exec(q)) {
-                hashParams[e[1]] = decodeURIComponent(e[2]);
-            }
-            return hashParams;
-        }
-
-        let params = getHashParams();
-
-        if (localStorage.access_token == 'undefined') {
-            let access_token = params.access_token,
-                refresh_token = params.refresh_token,
-                error = params.error;
-
-            localStorage.access_token = access_token;
-        }
-
-        if (!localStorage.access_token) {
-            let access_token = params.access_token,
-                refresh_token = params.refresh_token,
-                error = params.error;
-
-            localStorage.access_token = access_token;
-        }
-    }
-
-    /* Permanece o Token no LocaStorage */
-    TokenRemain()
-
-    const token = 'access_token='+ localStorage.access_token;
-    const InUserDetails = JSON.parse(localStorage.userDetails);
-
-    async function getUser() {
-        await api.get('/me?' + token)
-            .then(function (response) {
-                // handle success
-                console.log(response);
-                const userDetails = {
-                    userName: response.data.display_name,
-                    userImg: response.data.images[0]
-                }
-
-                localStorage.userDetails = JSON.stringify(userDetails);
-
-                return userDetails;
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .finally(function () {
-                // always executed
-            });
-    }
-
-    getUser();
+        let username = localStorage.userName;
+        let userimg = localStorage.userImg;
 
     return (
         <div className="AppHome">
@@ -76,8 +17,8 @@ export default function Home() {
                     <span>Xpotify</span>
                 </div>
                 <div className="user">
-                    <span className="userName">{InUserDetails.userName}</span>
-                    <img className="userImg" src={InUserDetails.userImg.url} alt="" />
+                    <span className="userName">{username}</span>
+                    <img className="userImg" src={userimg} alt="" />
                 </div>
             </header>
             <main className="HomeMain">
