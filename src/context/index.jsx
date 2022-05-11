4862cc6f0ec5cbec9cb12bdbd7e89b6e8ef6dd23
ArielBetti <ariel.betti@gmail.com>
2022-05-11 02:27:27 -0300

    feat: implementing context api

---
+++ src/context/index.jsx
@@ -0,0 +1,37 @@
+import { createContext, useEffect, useState, useContext } from "react";
+import { useRecoilState } from "recoil";
+import { atomToken } from "../store/atoms";
+import spotifyMethods from "../utils/spotifyMethods";
+
+export const XpotifyContext = createContext({});
+export const getNewToken = () => spotifyMethods.refreshToken();
+
+export const XpotifyProvider = ({ children }) => {
+  const [token, setToken] = useRecoilState(atomToken);
+  const [newToken, setNewToken] = useState("");
+
+  // constants
+  const TOKEN_IN_55_MIN = 3300000;
+
+  const refreshTokenValue = () => {
+    getNewToken().then((newToken) => setNewToken(`Bearer ${newToken}`));
+  };
+
+  useEffect(() => {
+    if (newToken && btoa(newToken)) {
+      setToken(newToken);
+    }
+  }, [newToken]);
+
+  useEffect(() => {
+    setInterval(() => refreshTokenValue(), TOKEN_IN_55_MIN);
+  }, [token]);
+
+  return (
+    <XpotifyContext.Provider value={{ refreshTokenValue }}>
+      {children}
+    </XpotifyContext.Provider>
+  );
+};
+
+export const useXpotifyContext = () => useContext(XpotifyContext);
