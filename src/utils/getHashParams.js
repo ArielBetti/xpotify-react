ad9ef31ac6383f5a7fa26bb6b338d9476cc9ccd9
ArielBetti <ariel.betti@gmail.com>
2022-05-11 02:21:17 -0300

    refac: mount auth token methods

---
+++ src/utils/getHashParams.js
@@ -0,0 +1,10 @@
+export const getHashParams = () => {
+  let hashParams = {};
+  let e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  while ((e = r.exec(q))) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+};
