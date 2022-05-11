0e063f1c466914b7f720170d9dc652036f2e508c
ArielBetti <ariel.betti@gmail.com>
2022-05-11 02:22:16 -0300

    feat: create a theme configuration

---
+++ src/theme/index.js
@@ -0,0 +1,65 @@
+export const light = {
+  colors: {
+    neutral: {
+      zero: "#ffffff",
+      pure: "#f5f5f5",
+      contrast: "#000000",
+      1: "#eeeeee",
+      2: "#e0e0e0",
+      3: "#eceff1",
+    },
+    text: {
+      white: "#FFF",
+      dark: "#000000",
+      constrast: "#000000",
+      primary: "#00d95f",
+    },
+    background: "#fafafa",
+    primary: "#00d95f",
+    secondary: "#2941ab",
+    contrast: "#805acb",
+  },
+  transitions: {
+    1: "0.35s",
+    2: "0.25",
+  },
+  breakpoints: {
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1350px",
+  },
+};
+
+export const dark = {
+  colors: {
+    neutral: {
+      zero: "#000000",
+      pure: "#212121",
+      contrast: "#ffffff",
+      1: "#1b1b1b",
+      2: "#373737",
+      3: "#494949",
+    },
+    text: {
+      white: "#ffffff",
+      dark: "#000000",
+      contrast: "#ffffff",
+      primary: "#00d95f",
+    },
+    background: "#212121",
+    primary: "#00d95f",
+    secondary: "#2941ab",
+    contrast: "#00d95f",
+  },
+  transitions: {
+    1: "0.35s",
+    2: "0.25",
+  },
+  breakpoints: {
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1350px",
+  },
+};
