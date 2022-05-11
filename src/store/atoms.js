6351939927d4a8e3aaf4351472b5692ce9807cb5
ArielBetti <ariel.betti@gmail.com>
2022-05-11 02:13:55 -0300

    feat: implementing recoiljs state manager in project

---
+++ src/store/atoms.js
@@ -0,0 +1,48 @@
+import { atom } from "recoil";
+
+// storage: localStorage
+import { localStorageEffect } from "../utils/localStorageEffect";
+
+export const atomToken = atom({
+  key: "Token",
+  default: undefined,
+  effects: [localStorageEffect("bearer")],
+});
+
+export const atomRefreshToken = atom({
+  key: "RefreshToken",
+  default: undefined,
+  effects: [localStorageEffect("refresh")],
+});
+
+export const atomUser = atom({
+  key: "User",
+  default: undefined,
+  effects: [localStorageEffect("current_user")],
+});
+
+export const atomTracks = atom({
+  key: "Tracks",
+  default: undefined,
+});
+
+export const atomArtist = atom({
+  key: "Artists",
+  default: undefined,
+});
+
+export const atomAlbums = atom({
+  key: "Albums",
+  default: undefined,
+});
+
+export const atomGlobalSearch = atom({
+  key: "GlobalSearch",
+  default: undefined,
+});
+
+export const atomDarkTheme = atom({
+  key: "DarkTheme",
+  default: false,
+  effects: [localStorageEffect("dark_mode")],
+});
