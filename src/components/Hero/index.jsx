73f286ddca4a42d2194f55dccb209047ea2a55de
ArielBetti <ariel.betti@gmail.com>
2022-05-12 18:27:21 -0300

    feat(hero-component): create hero-component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,16 @@
+// typography
+import * as Typography from '../../Typography';
+
+// atoms: components
+import * as Atom from './style';
+
+const Hero = ({ title, image }) => {
+  return (
+    <Atom.HeroContainer>
+      <Atom.HeroImg src={image} alt="Arte do artista/album" />
+      <Atom.TitleWrapper>{title}</Atom.TitleWrapper>
+    </Atom.HeroContainer>
+  );
+};
+
+export default Hero;
