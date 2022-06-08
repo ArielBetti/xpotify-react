ae2862ed57749b53372092f33aa0ee6603843fd4
ArielBetti <ariel.betti@gmail.com>
2022-06-08 12:20:55 -0300

    refac: login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,23 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
-import Logo from "../../assets/Logo";
 
+// recoil: atoms
 import { atomUser } from "../../store/atoms";
-import "./style.css";
 
+// icons
+import { MdLogin } from "react-icons/md";
+
+// illustration
+import { LoginIllustrationRender } from "../../assets/illustrations/loginIllustra";
+
+// atoms: components
+import * as Atom from "./style";
+
+// typography
+import * as Typography from "../../Typography";
+
+// ::
 const Login = () => {
   const navigateTo = useNavigate();
 
@@ -16,18 +28,59 @@ const Login = () => {
   }, [hasUser]);
 
   return (
-    <div className="AppLogin">
-      <div className="logo">
-        <Logo size="70px" />
-        <h1 className="TitleLogin">Xpotify</h1>
-      </div>
-      <a className="LoginBtn" href="https://xpotify-auth.herokuapp.com/login">
-        Login com Spotify{" "}
-      </a>
-      {/* <a className="LoginBtn" href="http://localhost:8080/login">
-        Login com Spotify{" "}
-      </a> */}
-    </div>
+    <Atom.LoginContainer>
+      <Atom.LoginButtonContainer>
+        <Atom.LoginButton href={process.env.REACT_APP_LOGIN_REQUEST_URL}>
+          <Typography.ParagraphLarge>
+            <Atom.LoginButtonContainer>
+              <MdLogin />
+              Entrar
+            </Atom.LoginButtonContainer>
+          </Typography.ParagraphLarge>
+        </Atom.LoginButton>
+      </Atom.LoginButtonContainer>
+      <LoginIllustrationRender />
+      <Atom.LoginDisclaimerContainer>
+        <Atom.LoginTitle>Bem vindo ao Xpotify!</Atom.LoginTitle>
+        <Typography.Paragraph>
+          Esse é um projeto desenvolvido para para explorar arquiteturas
+          front-end integrando as APIs do spotify você pode ver o código dessa
+          aplicação{" "}
+          <Atom.LoginExternalLink
+            target="_blank"
+            href="https://github.com/ArielBetti/Xpotify-React"
+          >
+            aqui
+          </Atom.LoginExternalLink>
+          .
+        </Typography.Paragraph>
+        <Typography.Paragraph>
+          Olá meu nome é Ariel Betti desenvolvedor desse projeto, se você gostou dessa aplicação considere
+          dar apoio seguindo nas minhas redes:{" "}
+          <Atom.LoginExternalLink
+            target="_blank"
+            href="https://github.com/ArielBetti"
+          >
+            GitHub
+          </Atom.LoginExternalLink>
+          ,{" "}
+          <Atom.LoginExternalLink
+            target="_blank"
+            href="https://www.linkedin.com/in/ariel-betti/"
+          >
+            LinkdIn
+          </Atom.LoginExternalLink>{" "}
+          e{" "}
+          <Atom.LoginExternalLink
+            target="_blank"
+            href="https://www.youtube.com/channel/UCXCyTeW1V33Ki4PyMLCn2zg"
+          >
+            Youtube
+          </Atom.LoginExternalLink>
+          .
+        </Typography.Paragraph>
+      </Atom.LoginDisclaimerContainer>
+    </Atom.LoginContainer>
   );
 };
 
---
+++ src/pages/Login/style.jsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import { ParagraphLarge } from "../../Typography";
+
+export const LoginContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 40px;
+`;
+
+export const LoginButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => props?.theme?.colors?.text?.contrast};
+  gap: 10px;
+  padding: 10px 20px;
+  text-decoration: none;
+
+  a {
+    color: ${(props) => props?.theme?.colors?.text?.contrast};
+  }
+
+`;
+
+export const LoginButton = styled.a`
+  display: flex;
+  transition: ${(props) => props.theme.transitions[2]};
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  border: 2px solid ${(props) => props?.theme?.colors?.contrast};
+  text-decoration: none;
+  cursor: pointer;
+  background-image: ${(props) =>
+    `linear-gradient(${props.theme?.colors?.contrast}, ${props.theme?.colors?.contrast})`};
+  background-size: 0%;
+  background-repeat: no-repeat;
+
+  :hover {
+    background-size: 100%;
+    color: ${(props) => props.theme.colors?.text?.white};
+  }
+`;
+
+export const LoginDisclaimerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  line-height: 1.4em;
+  max-width: 450px;
+`;
+
+export const LoginTitle = styled(ParagraphLarge)`
+  margin-bottom: 20px;
+`;
+
+export const LoginExternalLink = styled.a`
+  color: ${(props) => props?.theme?.colors?.contrast};
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+`;
