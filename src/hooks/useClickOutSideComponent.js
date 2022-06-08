178cdab7fc752e1b0570bbc2dcb54ba390944a87
ArielBetti <ariel.betti@gmail.com>
2022-06-08 12:22:43 -0300

    feat: create click-out-side hook

---
+++ src/hooks/useClickOutSideComponent.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export const useClickOutSideComponent = (ref, action) => {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        action && action();
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [action]);
+};
